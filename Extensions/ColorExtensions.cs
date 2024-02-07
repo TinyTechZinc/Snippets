ce0c9ca4c13fbdab6bbf6ee0e969d013ccad6996
Timothy Zink <timdzink@gmail.com>
2024-02-06 19:45:27 -0700

    add color extension

---
+++ Extensions/ColorExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.Versioning;
+
+namespace Extensions
+{
+	public static class ColorExtensions
+	{
+		[SupportedOSPlatform("windows")]
+		public static ImageAttributes BuildAttributes(this Color color)
+		{
+			ColorMatrix matrix = new(
+			[
+				[color.R / 255f, 0, 0, 0, 0],
+				[0, color.G / 255f, 0, 0, 0],
+				[0, 0, color.B / 255f, 0, 0],
+				[0, 0, 0, color.A / 255f, 0],
+				[0, 0, 0, 0, 1]
+			]);
+			ImageAttributes toReturn = new();
+			toReturn.SetColorMatrix(matrix);
+			return toReturn;
+		}
+	}
+}
--- Extensions/RandomExtensions.cs
+++ Extensions/RandomExtensions.cs
@@ -9,7 +9,7 @@ namespace Extensions
 			return Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
 		}
 		/// <summary>
-		/// Return a random sequence of these elements.
+		/// Return a random sequence of these elements using Durstenfeld's variant of the Fisher-Yates shuffle algorithm.
 		/// </summary>
 		/// <remarks>
 		/// Reference: https://stackoverflow.com/a/1287572/294804
@@ -36,25 +36,14 @@ namespace Extensions
 		/// Returns an IEnumerable of unique integers from min to max (inclusive) in a random order.
 		/// </summary>
 		/// <remarks>
-		/// Based on <see cref="Shuffle{T}(IEnumerable{T}, Random)"/>
-		/// Reference: https://stackoverflow.com/a/1287572/294804
-		/// By Jon Skeet
+		/// Uses <see cref="Shuffle{T}(IEnumerable{T}, Random)"/>
 		/// </remarks>
 		/// <param name="min">Minimum value (inclusive)</param>
 		/// <param name="max">Maximum value (inclusive)</param>
 		/// <returns></returns>
 		public static IEnumerable<int> PermuteIntegers(this Random random, int min, int max)
 		{
-			int[] elements = Enumerable.Range(min, max - min + 1).ToArray();
-			for (int i = elements.Length - 1; i >= 0; i--)
-			{
-				// Swap element "i" with a random earlier element it (or itself)
-				int swapIndex = random.Next(i + 1);
-				yield return elements[swapIndex];
-				elements[swapIndex] = elements[i];
-				// ... except we don't really need to swap it fully, as we can
-				// return it immediately, and afterwards it's irrelevant.
-			}
+			return random.Shuffle(Enumerable.Range(min, max - min + 1));
 		}
 	}
 }
